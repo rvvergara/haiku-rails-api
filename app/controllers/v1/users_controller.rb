15364ed224f9e8856afca5d71c89cf72b5b5b820
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-12 19:09:24 +0800

    Create users controller creation action

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,29 @@
+class V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+
+    if user
+      token = JsonWebToken.encode({ id: user.id })
+      render json: { user: user.attributes, token: token }, status: 201
+    else
+      render json: { message: 'Cannot create account', errors: user.errors }, status: 422
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :password,
+      :password_confirmation,
+      :role
+    )
+  end
+end
