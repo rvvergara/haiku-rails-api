8db311df45867fa44fe22e884f26ec4d41a223b8
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-14 18:45:00 +0800

    Add default time and date to availability model

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -5,9 +5,27 @@ class Availability < ApplicationRecord
 
   validates :availability_date, :start_time, :end_time, presence: true
 
+  before_validation :raise_conflict
+
   after_initialize do
     self.availability_date = Date.today
-    self.start_time = Time.now
-    self.end_time = Time.now + 2700
+    self.start_time = Time.now.strftime('%H:%M:%S')
+    self.end_time = (Time.now + 2700).strftime('%H:%M:%S')
+  end
+
+  private
+
+  def has_conflict?
+    Availability.where(
+      'availability_date = :date AND end_time < :start_time ',
+      date: availability_date,
+      start_time: start_time
+    )
+  end
+
+  def raise_conflict
+    return unless has_conflict?
+
+    errors.add(:end_time, :self, message: 'Conflicts with another availability slot.')
   end
 end
