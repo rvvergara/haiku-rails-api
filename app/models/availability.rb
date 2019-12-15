574b56119b9e8e4141f29fb430080c4dd4d45ca1
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-15 08:03:12 +0800

    Add custom validations for availability

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -5,27 +5,34 @@ class Availability < ApplicationRecord
 
   validates :availability_date, :start_time, :end_time, presence: true
 
-  before_validation :raise_conflict
+  before_validation :is_valid_range?, :raise_conflict
 
   after_initialize do
-    self.availability_date = Date.today
-    self.start_time = Time.now.strftime('%H:%M:%S')
-    self.end_time = (Time.now + 2700).strftime('%H:%M:%S')
+    current_time = Tod::TimeOfDay(Time.now)
+    self.availability_date = Date.today if self.availability_date.nil?
+    self.start_time = current_time.strftime('%H:%M:%S') if self.start_time.nil?
+    self.end_time = (current_time + 2700).strftime('%H:%M:%S') if self.end_time.nil?
   end
 
   private
 
+  def is_valid_range?
+    return if start_time <= end_time
+
+    errors[:base] << 'Start time must be earlier or same as end time.'
+  end
+
   def has_conflict?
-    Availability.where(
-      'availability_date = :date AND end_time < :start_time ',
-      date: availability_date,
-      start_time: start_time
-    )
+    same_day = Availability.where('availability_date=?', availability_date)
+    same_day.any? do |slot|
+      range = slot.start_time..slot.end_time
+      range.include?(start_time) || range.include?(end_time) 
+    end
   end
 
   def raise_conflict
     return unless has_conflict?
 
-    errors.add(:end_time, :self, message: 'Conflicts with another availability slot.')
+    errors[:base] << 'Time  slot has conflict with another record.'
   end
 end
