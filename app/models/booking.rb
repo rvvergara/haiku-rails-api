60caf166f42e0f750b6394da9802e68699bf1c29
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-15 17:54:08 +0800

    Add confirm and private methods to booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,7 +4,44 @@ class Booking < ApplicationRecord
 
   validates :start_time, presence: true
 
+  before_validation :check_if_slot_taken
+
+
+  def confirm
+    # set booking confirmed to true
+    update(confirmed: true)
+    # set booked column in availability record to true
+    slot = corresponding_open_slot
+    slot.update(booked: true)
+  end
+
+  def cancel
+    # set cancelled column to true
+
+    # set booked column in availability record to true
+  end
+
   private
 
-  
+  def check_if_slot_taken
+    return if corresponding_open_slot
+
+    errors[:time_slot] << 'already taken'
+  end
+
+  def corresponding_open_slot
+    availabilities = Availability.where(
+      'practitioner_id=? AND availability_date=? AND start_time<=? AND end_time>=? AND booked=?',
+      practitioner_id,
+      appointment_date,
+      start_time,
+      start_time,
+      false
+    )
+    availabilities[0]
+  end
+
+  def booked_slot
+    
+  end
 end
