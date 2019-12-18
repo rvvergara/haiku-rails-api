ea75cb676205c18812903dc133714e72b85f1de5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-19 07:55:19 +0800

    Configure users and sessions create controller actions
    1. In user create instead of immediately sending out user data and token
    after a successful creation, send a json response instead stating the
    need to verify an email
    2. In sessions create, add a conditional that checks whether an
    authenticated user has already been activated

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -5,8 +5,12 @@ class V1::SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
 
     if user &.valid_password?(params[:password])
-      token = JsonWebToken.encode(id: user.id)
-      render :create, locals: { user: user, token: token }, status: 200
+      if user.activated?
+        token = JsonWebToken.encode(id: user.id)
+        render :create, locals: { user: user, token: token }, status: 200
+      else
+        render json: { message: 'You need to first confirm your email. Please check your email for confirmation link provided when you signed up.'}, status: 401
+      end
     else
       render json: { error: 'Invalid Credentials' }, status: 401
     end
