048e7ab71506a4d9be01b8cc3909b93d95ae6761
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-20 08:14:48 +0800

    Write user model validation specs
    - Create factories with traits practitioner, patient and invalid_user
    - Write validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let (:ted) { build(:user, :patient) }
+    let (:nomad) { build(:user, :invalid_user) }
+    
+    context 'complete sign up info' do
+      it 'is valid' do
+        expect(ted).to be_valid
+      end
+    end
+
+    context 'no role given' do
+      it 'is invalid' do
+        expect(nomad).to_not be_valid
+        expect(nomad.errors['role']).to include("can't be blank")
+      end
+    end
+  end
 end
