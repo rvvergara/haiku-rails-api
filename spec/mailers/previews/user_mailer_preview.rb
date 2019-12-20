4f7e9f531ca8f544a2914e2243d8e01487439d20
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2019-12-20 19:42:14 +0800

    Create account_activation mailer preview

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def account_activation
+    user = FactoryBot.build(:user, email: 'doctor@example.com', role:'practitioner')
+    user.activation_token = User.new_token
+    UserMailer.account_activation(user)
+  end
 end
